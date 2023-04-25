c50ba019df98e153fc72c18d586fd326402d09a5
InfoMagix <95236282+InfoMagix@users.noreply.github.com>
2023-04-25 23:52:41 +0100

    timings

--- swift_auto_caller.py
+++ swift_auto_caller.py
@@ -33,7 +33,8 @@ folder_path = 'bird_calls'
 playback_schedule = [
     ("07:00", "08:00"),
     ("17:00", "18:00"),
-    ("23:40", "23:45"),    
+    ("23:40", "23:45"), 
+    ("23:55", "23:59"),        
 ]
 
 # Sort the playback_schedule in ascending order
